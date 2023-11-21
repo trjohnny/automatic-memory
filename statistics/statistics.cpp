51aafde45d85a8aee3c665c5f1075403a4ad39db
Giovanni <jacket.gav@gmail.com>
2023-11-21 11:21:24 +0100

    fix: optimize statistics calculation

--- statistics/statistics.cpp
+++ statistics/statistics.cpp
@@ -59,60 +59,52 @@ void Statistics::calculateStatistics() {
     size_t numColumns = dataMatrix.size();
     numericalStatistics.resize(numColumns);
     categoricalFrequencyCounts.resize(numColumns);
-    correlationMatrix = Eigen::MatrixXd(numColumns, numColumns);
+    correlationMatrix = Eigen::MatrixXd::Constant(numColumns, numColumns, std::numeric_limits<double>::quiet_NaN());
 
-    // Temporary storage for numerical data for calculations
-    std::vector<double> tempNumericalData;
+    std::vector<std::vector<double>> preprocessedNumericalData(numColumns);
 
+    // Preprocess data: separate numerical and categorical data, and check column types
     for (size_t i = 0; i < numColumns; ++i) {
-        tempNumericalData.clear();
-
-        // Determine if the column is numerical or categorical
         bool isNumerical = std::holds_alternative<double>(dataMatrix[i].front().value_or(0));
 
         for (const auto& val : dataMatrix[i]) {
-            if (val.has_value()) {
-                if (isNumerical) {
-                    tempNumericalData.push_back(std::get<double>(*val));
-                } else {
-                    std::string category = std::get<std::string>(*val);
-                    categoricalFrequencyCounts[i][category]++;
-                }
-            }
-        }
+            if (!val.has_value()) continue;
 
-        if (isNumerical) {
-            // Calculate numerical statistics using GSL
-            size_t dataSize = tempNumericalData.size();
-            gsl_sort(tempNumericalData.data(), 1, dataSize);
-            numericalStatistics[i].mean = gsl_stats_mean(tempNumericalData.data(), 1, dataSize);
-            numericalStatistics[i].median = gsl_stats_median_from_sorted_data(tempNumericalData.data(), 1, dataSize);
-            numericalStatistics[i].variance = gsl_stats_variance(tempNumericalData.data(), 1, dataSize);
-            numericalStatistics[i].stdDev = gsl_stats_sd(tempNumericalData.data(), 1, dataSize);
+            if (isNumerical) {
+                preprocessedNumericalData[i].push_back(std::get<double>(*val));
+            } else {
+                std::string category = std::get<std::string>(*val);
+                categoricalFrequencyCounts[i][category]++;
+            }
         }
     }
 
-    // Calculate correlation matrix
+    // Calculate statistics for numerical data
     for (size_t i = 0; i < numColumns; ++i) {
-        for (size_t j = 0; j < numColumns; ++j) {
-            if (i != j && std::holds_alternative<double>(dataMatrix[i].front().value_or(0))
-                && std::holds_alternative<double>(dataMatrix[j].front().value_or(0))) {
-                std::vector<double> columnJData;
-                for (const auto& val : dataMatrix[j]) {
-                    if (val.has_value()) {
-                        columnJData.push_back(std::get<double>(*val));
-                    }
-                }
-                correlationMatrix(i, j) = gsl_stats_correlation(
-                        tempNumericalData.data(), 1,
-                        columnJData.data(), 1,
-                        tempNumericalData.size());
-            } else {
-                correlationMatrix(i, j) = std::numeric_limits<double>::quiet_NaN();
+        if (!preprocessedNumericalData[i].empty()) {
+            size_t dataSize = preprocessedNumericalData[i].size();
+            gsl_sort(preprocessedNumericalData[i].data(), 1, dataSize);
+
+            numericalStatistics[i].mean = gsl_stats_mean(preprocessedNumericalData[i].data(), 1, dataSize);
+            numericalStatistics[i].median = gsl_stats_median_from_sorted_data(preprocessedNumericalData[i].data(), 1, dataSize);
+            numericalStatistics[i].stdDev = gsl_stats_sd(preprocessedNumericalData[i].data(), 1, dataSize);
+            numericalStatistics[i].variance = numericalStatistics[i].stdDev * numericalStatistics[i].stdDev;
+        }
+    }
+
+    // Calculate correlation matrix (upper triangle only)
+    for (size_t i = 0; i < numColumns; ++i) {
+        for (size_t j = i + 1; j < numColumns; ++j) {
+            if (!preprocessedNumericalData[i].empty() && !preprocessedNumericalData[j].empty()) {
+                correlationMatrix(i, j) = correlationMatrix(j, i) = gsl_stats_correlation(
+                        preprocessedNumericalData[i].data(), 1,
+                        preprocessedNumericalData[j].data(), 1,
+                        preprocessedNumericalData[i].size());
             }
         }
     }
 }
+
 
 void Statistics::outputResults() {
     // Implementation to output results to outputFile
